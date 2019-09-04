3f6ac344d936ae2a762b13b43e01d2ce5d8b0c97
Ly <lydialawli@gmail.com>
2019-09-04 11:58:43 +0700

    070104

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const array = require('./data.js')
+const calc = require('./calc.js')
 
-console.log(array)
+let prices = array.items.map(e =>  e.price)
+
+console.log('sum: ',calc.add(prices))
+console.log('dif: ',calc.subtract(prices))
