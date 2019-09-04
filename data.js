188beb233f33670e6f0f40f7c1f8518387c716b4
Ly <lydialawli@gmail.com>
2019-09-04 11:40:45 +0700

    070101

---
+++ data.js
@@ -0,0 +1,27 @@
+module.exports =  {
+    items: [{
+        name: 'Duplex with Garden',
+        price: 250,
+    },
+    {
+        name: 'Double Room Shared House',
+        price: 500,
+    },
+    {
+        name: 'Single Room Shared House',
+        price: 300,
+    },
+    {
+        name: 'Studio Lounge small',
+        price: 280,
+    },
+    {
+        name: 'Studio Lounge Big',
+        price: 300,
+    },
+    {
+        name: 'Single room private House',
+        price: 250
+    },]
+    
+}
